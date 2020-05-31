dfb2f49e387b5017ab5e0fa1c8d393951320db14
Subhasish <subhasish469@gmail.com>
2020-05-31 12:52:02 +0530

    login validation

---
+++ login_app_user.php
@@ -0,0 +1,19 @@
+<?php
+
+$dc=new mysqli("localhost:3307","root","","online_store_db");
+$check_login_info=$dc->prepare("select*from app_users_table where email=? and pass=?");
+$check_login_info->bind_param("ss", $_GET["email"],$_GET["pass"]);
+$check_login_info->execute();
+
+$login_result=$check_login_info->get_result();
+
+if($login_result->num_rows==0){
+    echo "User does not exist";
+    
+}else{
+    echo "user does exist";
+}
+
+
+        
+
